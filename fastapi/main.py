0644e582eef675d0ad91aab5631aee8405ae2216
shinjs <tlswjdtjq00@skuniv.ac.kr>
2024-12-27 17:58:21 +0900

    디테일 맵 마커 성공

--- fastapi/main.py
+++ fastapi/main.py
@@ -13,7 +13,7 @@ from user import router as user_router
 from place import router as place_router
 from restaurant import router as restaurant_router
 from rating import router as rating_router
-
+from parking import router as parking_router
 
 app = FastAPI()
 
@@ -59,7 +59,7 @@ app.include_router(user_router, tags=["User Login"])
 app.include_router(restaurant_router, tags=["Restaurant"], prefix='/restaurant')
 app.include_router(place_router, tags=["Place"], prefix='/place')
 app.include_router(rating_router, tags=["Rating"], prefix='/rating')
-
+app.include_router(parking_router, tags=["Parking"],prefix="/parking")
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host = "0.0.0.0", port = 8000)
